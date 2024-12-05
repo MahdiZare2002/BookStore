cc45a248bd61a3c1f4acbac08c678c25b59b1f25
Mahdi <mahdi.zed2002@gmail.com>
2024-12-06 00:48:55 +0330

    Create AuthorService

---
+++ Core/Services/AuthorService/AuthorService.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories.AuthorRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.AuthorService
+{
+    public class AuthorService
+    {
+        private readonly AuthorRepository _repository;
+        public AuthorService(AuthorRepository authorRepository) 
+        {
+            _repository = authorRepository;
+        }
+
+        public async Task<IEnumerable<Author>> GetAllAuthors()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task CreateAuthor(Author author)
+        {
+            await _repository.Create(author);
+        }
+    }
+}
