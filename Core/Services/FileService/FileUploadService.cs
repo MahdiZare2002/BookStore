bb734effcd6ab8f6be6678ab7124c329ec61446f
Mahdi <mahdi.zed2002@gmail.com>
2024-12-08 20:34:18 +0330

    Handle FileUploadService

---
+++ Core/Services/FileService/FileUploadService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.FileService
+{
+    public class FileUploadService
+    {
+        private readonly string _storagePath;
+        public FileUploadService(IConfiguration configuration)
+        {
+            _storagePath = configuration["FileUpload:StoragePath"];
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            if (!Directory.Exists(_storagePath))
+            {
+                Directory.CreateDirectory(_storagePath);
+            }
+
+
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("File is empty or not provided.");
+            }
+
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var fullPath = Path.Combine(_storagePath, fileName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return fileName;
+
+        }
+    }
+}
