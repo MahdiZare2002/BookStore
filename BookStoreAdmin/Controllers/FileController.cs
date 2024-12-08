5162c735339106804ac0e5470cb1c15335cf6094
Mahdi <mahdi.zed2002@gmail.com>
2024-12-09 00:36:01 +0330

    Create api for get images at storage path

---
+++ BookStoreAdmin/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminBookShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly string _storagePath;
+
+        public FileController(IConfiguration configuration)
+        {
+            _storagePath = configuration["FileUpload:StoragePath"];
+        }
+
+        [HttpGet("GetFile")]
+        public IActionResult DownloadFile(string fileName)
+        {
+            var fullPath = Path.Combine(_storagePath, fileName);
+
+            if (!System.IO.File.Exists(fullPath))
+            {
+                return NotFound("File not found.");
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(fullPath);
+            var contentType = "application/octet-stream";
+
+            return PhysicalFile(fullPath, contentType, fileName);
+        }
+    }
+}
