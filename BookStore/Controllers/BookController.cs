9e61e3a9dc906c27458bc813c476b3e9c921725d
Mahdi <mahdi.zed2002@gmail.com>
2024-12-14 00:24:33 +0330

    create BookController to handle bookdetails

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Core.Services.BookService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookService _bookService;
+
+        public BookController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var data = await _bookService.GetBookById(id);
+            return View(data);
+        }
+    }
+}
